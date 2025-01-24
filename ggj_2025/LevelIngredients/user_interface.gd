504ebae51bf0749c2bcd0b948eb9cf70a2ec172a
ricklusuardi <lusuardiriccardo@gmail.com>
2025-01-25 00:07:17 +0100

    set up ui that shows size of bubble bases

--- ggj_2025/LevelIngredients/user_interface.gd
+++ ggj_2025/LevelIngredients/user_interface.gd
@@ -11,8 +11,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if main_level.game_over == false:
 		_update_score()
+		_update_progress_bars() 
 
 func _update_score() -> void:
 	var cleaned_score = int(main_level.score)
-	$Label.text = "SCORE: " + str(cleaned_score)
+	$VBoxContainer/Score.text = "SCORE: " + str(cleaned_score)
 	pass
+
+func _update_progress_bars() -> void:
+	for i in main_level.bubble_bases:
+		pass
+	$VBoxContainer/VBoxContainer/BubbleBase1Size.value = main_level.bubble_bases[0].scale.x*100
+	$VBoxContainer/VBoxContainer2/BubbleBase2Size.value = main_level.bubble_bases[1].scale.x*100
+	$VBoxContainer/VBoxContainer3/BubbleBase3Size.value = main_level.bubble_bases[2].scale.x*100
