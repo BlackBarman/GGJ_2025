7be789b66f1270dc236ef411cddc869c57dc0a37
ricklusuardi <lusuardiriccardo@gmail.com>
2025-01-24 22:40:39 +0100

    implemented basic behaviour of BubbleBase

--- ggj_2025/LevelIngredients/bubble_base.gd
+++ ggj_2025/LevelIngredients/bubble_base.gd
@@ -1,6 +1,12 @@
 extends Node3D
 
+
 class_name BubbleBase
+
+
+@export var size_change_speed : float = 0.1
+@export var max_size : float = 3.0
+@export var min_size : float = 0.4
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +15,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	_increase_size(delta)
+
+
+func _increase_size(delta: float) -> void: 
+	if scale.x < max_size:
+		scale.x += (size_change_speed * delta)
+		scale.y += (size_change_speed * delta)
+		scale.z += (size_change_speed * delta)
+
+
+func _decrease_size(damage : float) -> void:
+	scale.x -= damage
+	scale.y -= damage
+	scale.z -= damage
+	if scale.x <= min_size:
+		queue_free() 
+	
