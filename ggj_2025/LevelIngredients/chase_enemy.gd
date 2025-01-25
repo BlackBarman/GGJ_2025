17e2568655a3932d2594e538205fc654c5f14a62
marketigerinfo <rick.rijkers1@gmail.com>
2025-01-25 17:00:46 +0100

    Added enemy which attacks player

---
+++ ggj_2025/LevelIngredients/chase_enemy.gd
@@ -0,0 +1,72 @@
+extends CharacterBody3D
+@export var damage_to_bubble = 10
+@export var nav_speed = 2
+var target_bubble = null
+@onready var nav_agent = $NavigationAgent3D
+var following_player=false
+
+func _ready() -> void:
+	_get_new_bubble()
+
+func _get_new_bubble():
+	var bubbles = get_tree().get_nodes_in_group("BubbleBaseGroup")
+	var closest_distance = 10000000
+	var closest_bubble = null
+	
+	for b in bubbles:
+		var dis = global_position.distance_to(b.global_position)
+		if dis < closest_distance:
+			closest_distance = dis
+			closest_bubble = b
+	
+	if closest_bubble != null:
+		target_bubble = closest_bubble
+		update_target_location(target_bubble.position)
+	else:
+		target_bubble =null
+
+func _physics_process(delta: float) -> void:	
+	if nav_agent.is_target_reachable():
+		return
+	else:
+		if !following_player:
+			_get_new_bubble()
+		
+	if target_bubble ==null:
+		_get_new_bubble()
+		return
+		
+	if !following_player:
+		var bubble_size = target_bubble.get_global_transform().basis.x
+		var current_location = global_transform.origin
+		var next_location = nav_agent.target_position 
+		look_at(next_location)
+		rotation.x =0
+		rotation.z =0
+		var distance_to_bubble = nav_agent.distance_to_target()
+		
+		if distance_to_bubble - 0.7 < bubble_size.x:
+			velocity = Vector3(0,0,0)  
+			target_bubble._decrease_size(delta/damage_to_bubble)
+		else:
+			var new_velocity = (next_location - current_location).normalized() *nav_speed
+			velocity = new_velocity 
+			move_and_slide()
+	else:
+		update_target_location(target_bubble.global_position)
+		var current_location = global_transform.origin
+		var next_location = nav_agent.target_position 
+		look_at(next_location)
+		var new_velocity = (next_location - current_location).normalized() *nav_speed
+		velocity = new_velocity 
+		move_and_slide()
+	
+func update_target_location(target_location):
+	nav_agent.target_position=target_location
+
+
+func _on_detection_collider_body_entered(body: Node3D) -> void:
+	if  body.name == "CharacterBody3D": 
+		following_player =true
+		target_bubble =body
+		update_target_location(body.global_position)
--- ggj_2025/LevelIngredients/enemy_manager.gd
+++ ggj_2025/LevelIngredients/enemy_manager.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
 @export var enemy_scene: PackedScene
+@export var chase_enemy_scene: PackedScene
 
-@export var enemy_spawn_time = 1
-
+@export var enemy_spawn_time = 0.5
+@export var enemies_to_spawn = 3;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,10 +19,16 @@ func _process(delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
-	var enemy = enemy_scene.instantiate()
-	var path_length = $SpawnLocation.curve.get_baked_length()
-	var offset = randf_range(0.0, path_length)
-	var enemy_spawn_location = $SpawnLocation.curve.sample_baked(offset)
+	for n in enemies_to_spawn:
+		var enemy = null
+		var randInt = randi_range(0, 100)
+		if(randInt >70):
+			enemy= chase_enemy_scene.instantiate()
+		else:
+			enemy= enemy_scene.instantiate()
+		var path_length = $SpawnLocation.curve.get_baked_length()
+		var offset = randf_range(0.0, path_length)
+		var enemy_spawn_location = $SpawnLocation.curve.sample_baked(offset)
 
-	enemy.global_position = global_transform.basis * enemy_spawn_location
-	add_child(enemy)
+		enemy.global_position = global_transform.basis * enemy_spawn_location
+		add_child(enemy)
