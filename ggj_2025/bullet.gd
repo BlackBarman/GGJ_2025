e4bf4287252f92f40ca1aa0000f0645d1cae00a9
sebastiano zezza <sebastiano.zezza@gmail.com>
2025-01-25 03:55:03 +0100

    Bullets now die when they exit the screen or collide with enemies
    also fixed bug not allowing the full use of WASD keys

--- ggj_2025/bullet.gd
+++ ggj_2025/bullet.gd
@@ -6,7 +6,18 @@ class_name BaseBullet
 var bullet_dir : Vector3
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready() -> void:
+	%Timer.start()
+
 func _process(delta: float) -> void:
 	position += bullet_dir * bullet_speed * delta
-	pass
+
+func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("EnemyGroup"):
+		queue_free()
+
+func _on_timer_timeout() -> void:
+	queue_free()
