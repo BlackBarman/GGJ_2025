3516d69ff6bd9515b30742594952c65b58d2e127
sebastiano zezza <sebastiano.zezza@gmail.com>
2025-01-25 15:42:46 +0100

    Added audio manager global class

---
+++ ggj_2025/Globals/AudioManager.gd
@@ -0,0 +1,5 @@
+extends Node
+
+##this is a global, in your scripts simply call the global class,
+##the name of the audio file and add. play 
+## ie : AudioManager.BuildTower.play() (from any script)
