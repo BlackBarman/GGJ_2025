402e7aaba78b2836e2574368312353486e6472bf
sebastiano zezza <sebastiano.zezza@gmail.com>
2025-01-25 15:31:12 +0100

    fixed 5000+ projectiles red errors
    aoldjaljd

--- ggj_2025/weapon.gd
+++ ggj_2025/weapon.gd
@@ -6,13 +6,23 @@ extends Node3D
 
 @export var bullet_spawn_pos : Node3D
 
+@onready var front_spawn = %bullet_spawn_point
+@onready var back_spawn  = %bullet_spawn_point2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("shoot"):
-		var bull = bullet.instantiate() as BaseBullet
-		bull.global_position = bullet_spawn_pos.global_position
+		call_deferred("shoot")
 		
-		bull.bullet_dir = (%bullet_spawn_point.global_position - %bullet_spawn_point2.global_position).normalized()
-		bull.bullet_dir.y = 0
-		get_tree().root.add_child(bull)
+		
+
+
+
+func shoot():
+	var bull = bullet.instantiate() as BaseBullet
+	get_tree().root.add_child(bull)
+	
+	bull.global_position = bullet_spawn_pos.global_position
+	bull.bullet_dir = (front_spawn.global_position - back_spawn.global_position).normalized()
+	bull.bullet_dir.y = 0
+	pass
