8c807ad15a3f708714a63bad866bbb1fb8537684
sebastiano zezza <sebastiano.zezza@gmail.com>
2025-01-25 09:56:21 +0100

    enemis can be killed by projectiles (theoretically)

--- ggj_2025/health_component.gd
+++ ggj_2025/health_component.gd
@@ -3,6 +3,6 @@ class_name HealthComponent
 
 
 func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("EnemyGroup"):
+	if body.is_in_group("PlayerBullet"):
 		get_parent().queue_free()
 	pass # Replace with function body.
