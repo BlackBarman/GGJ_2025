91afa1fc54ae4a88855e70d6d99b8bc2c75a3e14
ricklusuardi <lusuardiriccardo@gmail.com>
2025-01-25 18:50:49 +0100

    added control explanations to the pause menu

--- ggj_2025/Levels/pause_screen.gd
+++ ggj_2025/Levels/pause_screen.gd
@@ -4,6 +4,7 @@ extends ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
+	$"../ControlsExplBackground".visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,9 +19,18 @@ func _process(delta: float) -> void:
 
 
 func _on_resume_button_pressed() -> void:
+	$"../ControlsExplBackground".visible = false
 	get_tree().paused = false
 	visible = false
 
 
 func _on_restart_button_pressed() -> void:
+	$"../ControlsExplBackground".visible = false
 	LevelChanger._reload_level()
+
+
+func _on_show_controls_button_pressed() -> void:
+	if $"../ControlsExplBackground".visible == false:
+		$"../ControlsExplBackground".visible = true
+	else:
+		$"../ControlsExplBackground".visible = false
