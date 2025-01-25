2b1dd676e7a81abc010c304a8d62114a5eb41644
ricklusuardi <lusuardiriccardo@gmail.com>
2025-01-25 21:23:24 +0100

    proper implementation of the camera movement

--- ggj_2025/Levels/camera_3d.gd
+++ ggj_2025/Levels/camera_3d.gd
@@ -2,7 +2,7 @@ extends Camera3D
 
 
 @export var character : CharacterBody3D
-@export var speed : float = 15.0
+@export var speed : float = 3.0
 @export var main_level : Level
 
 # Called when the node enters the scene tree for the first time.
@@ -13,11 +13,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if main_level.game_over == false:
-		var hor_position = Vector2(global_position.x,global_position.z)
-		var hor_target = Vector2(character.global_position.x, character.global_position.z)
-		print("target: ", hor_target)
-		#hor_position.lerp(hor_target, speed)
-		hor_position = hor_target
-		print("current position: ", hor_position)
-		global_position.x = hor_position.x
-		global_position.z = hor_position.y
+		
+		var new_position = global_position.lerp(character.global_position, speed*delta)
+		global_position.x = new_position.x
+		global_position.z = new_position.z
