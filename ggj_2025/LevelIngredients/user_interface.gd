4137e71386eede9ddd67acbd5579076ed3d7b08e
sebastiano zezza <sebastiano.zezza@gmail.com>
2025-01-26 11:08:07 +0100

    added small timer ui

--- ggj_2025/LevelIngredients/user_interface.gd
+++ ggj_2025/LevelIngredients/user_interface.gd
@@ -5,6 +5,10 @@ extends Control
 @export var bubble_base_2 : BubbleBase
 @export var bubble_base_3 : BubbleBase
 
+var time_elapsed = 1
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$GameOverScreen.visible = false
@@ -27,7 +31,7 @@ func _update_progress_bars() -> void:
 		$VBoxContainer/VBoxContainer2/BubbleBase2Size.value = bubble_base_2.scale.x*100
 	if is_instance_valid(bubble_base_3):
 		$VBoxContainer/VBoxContainer3/BubbleBase3Size.value = bubble_base_3.scale.x*100
-
+	$VBoxContainer/Score2.text = "Time: " + str(time_elapsed)
 
 func _reveal_game_over_screen() -> void:
 	$GameOverScreen.visible = true
@@ -37,3 +41,9 @@ func _reveal_game_over_screen() -> void:
 
 func _on_try_again_button_pressed() -> void:
 	LevelChanger._reload_level()
+
+
+func _on_timer_timeout() -> void:
+	time_elapsed+=1
+	_update_progress_bars()
+	pass # Replace with function body.
