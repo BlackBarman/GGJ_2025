6bf3ce49439b32957b6d2d496b6ecf84532aa62e
ricklusuardi <lusuardiriccardo@gmail.com>
2025-01-26 01:16:14 +0100

    slowed down rate of fire, changed SoundManager to only have base sounds (not 3D), and added sound to gunshots

--- ggj_2025/Globals/AudioManager.gd
+++ ggj_2025/Globals/AudioManager.gd
@@ -3,3 +3,5 @@ extends Node
 ##this is a global, in your scripts simply call the global class,
 ##the name of the audio file and add. play 
 ## ie : AudioManager.BuildTower.play() (from any script)
+
+@onready var Gunshot = $Gunshot
--- ggj_2025/weapon.gd
+++ ggj_2025/weapon.gd
@@ -9,6 +9,8 @@ extends Node3D
 @onready var front_spawn = %bullet_spawn_point
 @onready var back_spawn  = %bullet_spawn_point2
 
+var can_shoot = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("shoot"):
@@ -19,10 +21,18 @@ func _process(_delta: float) -> void:
 
 
 func shoot():
-	var bull = bullet.instantiate() as BaseBullet
-	get_tree().root.add_child(bull)
-	
-	bull.global_position = bullet_spawn_pos.global_position
-	bull.bullet_dir = (front_spawn.global_position - back_spawn.global_position).normalized()
-	bull.bullet_dir.y = 0
-	pass
+	if can_shoot == true:
+		var bull = bullet.instantiate() as BaseBullet
+		get_tree().root.add_child(bull)
+		
+		bull.global_position = bullet_spawn_pos.global_position
+		bull.bullet_dir = (front_spawn.global_position - back_spawn.global_position).normalized()
+		bull.bullet_dir.y = 0
+		AudioManager.Gunshot.play()
+		can_shoot = false
+		$Timer.start()
+		
+
+
+func _on_timer_timeout() -> void:
+	can_shoot = true
