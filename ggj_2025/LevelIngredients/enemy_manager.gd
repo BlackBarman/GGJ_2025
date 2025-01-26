34431a89be4d417db97f5d68a769713dad02c0a6
marketigerinfo <rick.rijkers1@gmail.com>
2025-01-26 12:35:05 +0100

    Increase spawn count

--- ggj_2025/LevelIngredients/enemy_manager.gd
+++ ggj_2025/LevelIngredients/enemy_manager.gd
@@ -10,7 +10,7 @@ extends Node3D
 func _ready() -> void:
 	$Timer.wait_time = enemy_spawn_time
 	$Timer.start()
-
+	$IncreaseTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,16 +19,23 @@ func _process(delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
-	for n in enemies_to_spawn:
-		var enemy = null
-		var randInt = randi_range(0, 100)
-		if(randInt >70):
-			enemy= chase_enemy_scene.instantiate()
-		else:
-			enemy= enemy_scene.instantiate()
-		var path_length = $SpawnLocation.curve.get_baked_length()
-		var offset = randf_range(0.0, path_length)
-		var enemy_spawn_location = $SpawnLocation.curve.sample_baked(offset)
-		add_child(enemy)
+	var enemies = get_tree().get_nodes_in_group("EnemyGroup")
+	
+	if enemies.size() < 120:
+		for n in enemies_to_spawn:
+			var enemy = null
+			var randInt = randi_range(0, 100)
+			if(randInt >70):
+				enemy= chase_enemy_scene.instantiate()
+			else:
+				enemy= enemy_scene.instantiate()
+			var path_length = $SpawnLocation.curve.get_baked_length()
+			var offset = randf_range(0.0, path_length)
+			var enemy_spawn_location = $SpawnLocation.curve.sample_baked(offset)
+			add_child(enemy)
 
-		enemy.global_position = global_transform.basis * enemy_spawn_location
+			enemy.global_position = global_transform.basis * enemy_spawn_location
+
+
+func _on_increase_timer_timeout() -> void:
+	enemies_to_spawn += 1
